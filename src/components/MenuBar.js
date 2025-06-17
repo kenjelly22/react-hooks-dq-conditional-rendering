e345b6ac3cf9b2e35995ecd3754dd01109af4f97
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-17 13:50:12 -0400

    Remove duplicate instructions from alternate code

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -18,16 +18,6 @@ function MenuBar({setSelectedTab, selectedTab}) {
   // import React from "react"
 
   // function MenuBar({ selectedTab, setSelectedTab }) {
-  //   /*
-
-  //   The 'span' tags below are the menu items. Think about the way a menu
-  //   should work. When you click a menu item, the button typically becomes
-  //   'active' to indicate that it is currently selected. How could we achieve
-  //   this programatically? What other behavior do we expect when we click
-  //   on a menu item? Do we need state in this component, and if not, how can
-  //   this component be made aware of what is currently the active menu item?
-
-  //   */
 
   //   const icons = ["user", "photo", "cocktail", "themeisle"]
 
